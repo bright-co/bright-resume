c49a6441f0a4b7d7342852f3c057ebeee4350d26
Erfan Seidipoor <erfan.seidipoor@gmail.com>
2024-09-14 12:58:09 +0330

    log resume creation 4

--- apps/back/src/app/modules/pdf/pdf.service.ts
+++ apps/back/src/app/modules/pdf/pdf.service.ts
@@ -11,58 +11,63 @@ export class PdfService {
   async generatePdf(fileId: string, resumeId: string) {
     console.log(1);
 
-    const browser = await puppeteer.launch({
-      headless: "new",
-      args: ["--no-sandbox"],
-      ignoreDefaultArgs: ["--disable-extensions"],
-    });
+    try {
+      const browser = await puppeteer.launch({
+        headless: "new",
+        args: ["--no-sandbox"],
+        ignoreDefaultArgs: ["--disable-extensions"],
+      });
 
-    console.log(2);
+      console.log(2);
 
-    const page = await browser.newPage();
-    console.log(3);
-    await page.setJavaScriptEnabled(false);
-    console.log(4);
+      const page = await browser.newPage();
+      console.log(3);
+      await page.setJavaScriptEnabled(false);
+      console.log(4);
 
-    console.log({
-      resumeAddress:
+      console.log({
+        resumeAddress:
+          this.configService.get(EnvironmentVariablesEnum.RESUME_URL) +
+          "/resume/" +
+          resumeId,
+      });
+
+      await page.goto(
         this.configService.get(EnvironmentVariablesEnum.RESUME_URL) +
-        "/resume/" +
-        resumeId,
-    });
+          "/resume/" +
+          resumeId
+      );
 
-    await page.goto(
-      this.configService.get(EnvironmentVariablesEnum.RESUME_URL) +
-        "/resume/" +
-        resumeId
-    );
+      console.log("page.emulateMediaType");
+      await page.emulateMediaType("print");
 
-    console.log("page.emulateMediaType");
-    await page.emulateMediaType("print");
+      const filename = fileId + ".pdf";
+      const path = join(process.cwd(), "./files/" + filename);
 
-    const filename = fileId + ".pdf";
-    const path = join(process.cwd(), "./files/" + filename);
+      console.log({ path });
 
-    console.log({ path });
+      await page.pdf({
+        path,
+        format: "A4",
+        printBackground: true,
+        width: "21cm",
+        height: "29.7cm",
+        margin: {
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: 0,
+        },
+      });
 
-    await page.pdf({
-      path,
-      format: "A4",
-      printBackground: true,
-      width: "21cm",
-      height: "29.7cm",
-      margin: {
-        top: 0,
-        right: 0,
-        bottom: 0,
-        left: 0,
-      },
-    });
+      console.log("page.pdf");
 
-    console.log("page.pdf");
+      await browser.close();
 
-    await browser.close();
-
-    return path;
+      return path;
+    } catch (error) {
+      console.log({ error });
+      throw error;
+    }
   }
 }
